25c44266adb03618fc1ab28367cb380809e52abb
Ferryal Fajar <ferryal21@gmail.com>
2026-02-19 16:26:58 +0700

    feat: add components dropdown

---
+++ src/stories/Dropdown.stories.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Meta, StoryFn } from "@storybook/react";
+import { Dropdown } from "./Dropdown";
+
+export default {
+  title: "Dropdown",
+  component: Dropdown,
+
+  argTypes: {
+    multiple: {
+      control: "boolean",
+    },
+    withSearch: {
+      control: "boolean",
+    },
+    outlined: {
+      control: "boolean",
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "flex-start",
+          padding: "20px",
+          background: "#f9f9f9",
+          borderRadius: "0px",
+          boxSizing: "border-box",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta<typeof Dropdown>;
+
+const Template: StoryFn<typeof Dropdown> = (args) => <Dropdown {...args} />;
+
+export const SelectDropdownField = Template.bind({});
+SelectDropdownField.args = {
+  label: "Label",
+  options: [
+    { label: "Option 1", value: "option1" },
+    { label: "Option with icon", value: "optionwithicon" },
+    { label: "Long Long Option 3", value: "option3" },
+    { label: "Long Long Long Option 4", value: "option4" },
+    { label: "Long Long Long Long Option 5", value: "option5" },
+    { label: "Long Long Long Long Long Option 6", value: "option6" },
+  ],
+  multiple: false,
+  withSearch: true,
+  outlined: false,
+};
---
+++ src/stories/Dropdown.tsx
@@ -0,0 +1,171 @@
+import { useState } from "react";
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+type DropdownProps = {
+  label: string;
+  options: Option[];
+  multiple?: boolean;
+  onChange: (selected: Option[] | Option | null) => void;
+  withSearch: boolean;
+  outlined?: boolean;
+};
+
+export const Dropdown = ({
+  label,
+  options,
+  multiple = false,
+  onChange,
+  withSearch = true,
+  outlined = false,
+}: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selected, setSelected] = useState<Option[] | Option | null>(
+    multiple ? [] : null
+  );
+
+  const handleOptionClick = (option: Option) => {
+    if (multiple) {
+      const currentSelected = Array.isArray(selected) ? selected : [];
+      const isSelected = currentSelected.find((o) => o.value === option.value);
+      const updatedSelection = isSelected
+        ? currentSelected.filter((o) => o.value !== option.value)
+        : [...currentSelected, option];
+      setSelected(updatedSelection);
+      onChange(updatedSelection);
+    } else {
+      setSelected(option);
+      onChange(option);
+      setIsOpen(false);
+    }
+  };
+
+  const handleRemoveOption = (option: Option) => {
+    setSelected((prevSelected) =>
+      (prevSelected as Option[]).filter((o) => o.value !== option.value)
+    );
+    onChange((selected as Option[]).filter((o) => o.value !== option.value));
+  };
+
+  const isSelected = (option: Option) => {
+    if (multiple) {
+      return (
+        Array.isArray(selected) &&
+        selected.some((o) => o.value === option.value)
+      );
+    }
+    return (selected as Option)?.value === option.value;
+  };
+
+  const highlightMatch = (text: string, query: string) => {
+    if (!query) return text;
+
+    const regex = new RegExp(`(${query})`, "gi");
+    const parts = text.split(regex);
+
+    return parts.map((part, index) =>
+      regex.test(part) ? (
+        <span key={index} className="bg-green-700">
+          {part}
+        </span>
+      ) : (
+        part
+      )
+    );
+  };
+
+  return (
+    <div className="flex  gap-4 w-full">
+      <label className="text-sm font-medium text-gray-700 min-w-[100px] items-center">
+        {label}
+      </label>
+
+      <div className="relative flex-1">
+        <div
+          className={`border ${
+            outlined
+              ? "border-gray-300 bg-gray-100"
+              : "border-gray-300 bg-white"
+          } rounded px-3 py-2 flex items-center justify-between cursor-pointer text-black`}
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <div className="flex flex-wrap gap-2 items-center w-full">
+            {multiple && Array.isArray(selected)
+              ? selected.map((option) => (
+                  <span
+                    key={option?.value}
+                    className="flex items-center bg-gray-200 text-gray-600 px-2 py-1 rounded-full text-sm"
+                  >
+                    {option?.label}
+                    <span
+                      // className="ml-2 text-gray-900 hover:text-gray-800 cursor-pointer material-icons"
+                      className="ml-2 text-gray-600 hover:text-gray-700 cursor-pointer rounded-full bg-transparent border-[1.5px] h-4 w-4 flex items-center justify-center text-xs font-semibold"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleRemoveOption(option);
+                      }}
+                    >
+                      x
+                    </span>
+                  </span>
+                ))
+              : (selected as Option | null)?.label || (
+                  <span className="text-gray-600">Select...</span>
+                )}
+          </div>
+          <span className="material-icons text-gray-500">arrow_drop_down</span>
+        </div>
+
+        {isOpen && (
+          <div className="absolute mt-2 border border-gray-300 bg-white rounded shadow-lg w-full max-h-60 overflow-y-auto z-20">
+            <div className="px-3 py-2 flex items-center border-b border-gray-100">
+              {withSearch && (
+                <span className="material-icons text-gray-500">search</span>
+              )}
+              <input
+                type="text"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="w-full ml-3 px-2 py-1 text-sm text-gray-700 border-none focus:ring-0 focus:outline-none"
+                placeholder="Search..."
+              />
+              {searchTerm && (
+                <span
+                  className="material-icons text-gray-400 hover:text-gray-600 cursor-pointer"
+                  onClick={() => setSearchTerm("")}
+                >
+                  close
+                </span>
+              )}
+            </div>
+            <ul className="py-2">
+              {options.length > 0 ? (
+                options.map((option) => (
+                  <li
+                    key={option.value}
+                    className={`px-4 py-2 cursor-pointer text-sm hover:bg-green-50 ${
+                      isSelected(option)
+                        ? "bg-green-100 text-black"
+                        : "text-gray-700"
+                    }`}
+                    onClick={() => handleOptionClick(option)}
+                  >
+                    {highlightMatch(option.label, searchTerm)}
+                  </li>
+                ))
+              ) : (
+                <li className="px-4 py-2 text-gray-500 text-sm">
+                  No options found
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
